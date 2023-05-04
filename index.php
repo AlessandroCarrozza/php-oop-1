a1736cb2e926a17a990e54bf320c02af6684c49b
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-04 16:15:18 +0200

    dichiarazione classe movie

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<?php
+
+class Movie {
+    public $title;
+    public $plot;
+    public $exitDate;
+    public $director;
+    public $genre;
+    public $language;
+    public $productionHouse;
+}
