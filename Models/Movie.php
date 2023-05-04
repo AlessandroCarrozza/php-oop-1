188f08cdc7675ebb8cf83ded04e10c6ff757104f
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-04 16:27:46 +0200

    organizzazione in relative cartelle

---
+++ Models/Movie.php
@@ -0,0 +1,18 @@
+<?php
+
+class Movie {
+    public $title;
+    public $exitDate;
+    public $director;
+    public $genre;
+    public $language;
+
+
+    function __construct($_title, $_exitDate, $_director, $_genre, $_language){
+        $this->title = $_title;
+        $this->exitDate = $_exitDate;
+        $this->director = $_director;
+        $this->genre = $_genre;
+        $this->language = $_language;
+    }
+}
--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 
 <?php
 
-class Movie {
-    public $title;
-    public $exitDate;
-    public $director;
-    public $genre;
-    public $language;
-
-
-    function __construct($_title, $_exitDate, $_director, $_genre, $_language){
-        $this->title = $_title;
-        $this->exitDate = $_exitDate;
-        $this->director = $_director;
-        $this->genre = $_genre;
-        $this->language = $_language;
-    }
-}
+require __DIR__ "./Models/Movie.php";
