6eb4960acae0e8934b6b89d2c31bee0d883fd6ad
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-04 16:20:43 +0200

    definizione di un costruttore

--- index.php
+++ index.php
@@ -3,10 +3,17 @@
 
 class Movie {
     public $title;
-    public $plot;
     public $exitDate;
     public $director;
     public $genre;
     public $language;
-    public $productionHouse;
+
+
+    function __construct($_title, $_exitDate, $_director, $_genre, $_language){
+        $this->title = $_title;
+        $this->exitDate = $_exitDate;
+        $this->director = $_director;
+        $this->genre = $_genre;
+        $this->language = $_language;
+    }
 }
