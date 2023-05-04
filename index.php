742155fb0f3745de1c945d02c7802f9c2d299c92
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-04 16:42:53 +0200

    creazione istanze e fix require

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 
 <?php
 
-require __DIR__ "./Models/Movie.php";
+// classe Movie 
+require __DIR__  . "./Models/Movie.php";
+
+
+// // creazione istanze della classe Movie
+$MovieOne = new Movie("Land of Mine", "2015/12/03", "Martin Zandvliet", "guerra", "tedesco");
+$MovieTwo = new Movie("Titanic", "1998/01/16", "James Cameron", "Dramma", "inglese");
+$MovieThree = new Movie("Il talento del calabrone", "2020/11/18", "Giacomo Cimini", "thriller", "italiano");
